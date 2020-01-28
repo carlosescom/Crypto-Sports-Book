e2b97c75c7b984a54ae20ae24997e6a8784c31e2
carlosescom <carlos.escom.ipn@gmail.com>
2020-01-28 00:50:21 -0600

    TypeError: web3.BigNumber is not a constructor

--- test/helpers/ether.js
+++ test/helpers/ether.js
@@ -1,5 +1,7 @@
-function ether (n) {
-  return new web3.BigNumber(web3.utils.toWei(n, 'ether'));
+const BigNumber = require('bignumber.js');
+
+function ether(n) {
+  return new BigNumber(web3.utils.toWei(n.toString(10), 'ether'));
 }
 
 module.exports = {
