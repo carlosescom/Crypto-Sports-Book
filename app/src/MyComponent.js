7ade293dffe2d5de2848d1ecc9d6a0b301d2f92d
carlosescom <carlos.escom.ipn@gmail.com>
2020-01-31 12:52:52 -0600

    -+ JSX/SportsBook

--- app/src/MyComponent.js
+++ app/src/MyComponent.js
@@ -9,9 +9,58 @@ import logo from "./logo.png";
 
 export default ({ accounts }) => (
   <div className="App">
-    <div>
+    <div className="container">
       <img src={logo} alt="drizzle-logo" />
-      <h1>Crypto Sports Book</h1>
+      <h1>Super Bowl LIV</h1>
+      <table style={{
+        fontSize: '3em',
+        padding: '1em',
+        width: '60%',
+        margin: '0 auto'
+      }}>
+        <thead>
+          <tr>
+            <th>
+              San Francisco
+            </th>
+              <th>
+                Kansas City
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style={{
+              backgroundColor: '#d12d2d',
+              color: '#d9c69f',
+              textShadow: '2px 2px 1px #000',
+              width: '50%'
+            }}>
+              <ContractData contract="SportsBook" method="SAN_FRANCISCO_49ERS_score" />
+            </td>
+            <td style={{
+              backgroundColor: '#ea0000',
+              color: '#fff',
+              textShadow: '2px 2px 1px #ffc800',
+              width: '50%'
+            }}>
+              <ContractData contract="SportsBook" method="KANSAS_CITY_CHIEFS_score" />
+            </td>
+          </tr>
+          <tr style={{
+            fontSize: '0.5em',
+          }}>
+            <td>
+              <strong>SF pool: </strong>
+              <ContractData contract="SportsBook" method="SAN_FRANCISCO_49ERS_pool" /> wei
+          </td>
+            <td>
+              <strong>KC pool: </strong>
+              <ContractData contract="SportsBook" method="KANSAS_CITY_CHIEFS_pool" /> wei
+          </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
 
     <div className="section">
@@ -20,87 +69,37 @@ export default ({ accounts }) => (
     </div>
 
     <div className="section">
-      <h2>SportsBook</h2>
-      <p>
-        This shows a simple ContractData component with no arguments, along with
-        a form to set its value.        
-    uint256 public minFee = 10 finney;
-    uint256 public minBet = 30 finney;
-    uint256 public precision = 1 ether;
-    uint256 public profit;
-    uint256 public totalPool;
-    uint256 public SAN_FRANCISCO_49ERS_pool;
-    uint256 public KANSAS_CITY_CHIEFS_pool;
-    uint32 public SAN_FRANCISCO_49ERS_bettors;
-    uint32 public KANSAS_CITY_CHIEFS_bettors;
-    uint8 public SAN_FRANCISCO_49ERS_score;
-    uint8 public KANSAS_CITY_CHIEFS_score;
-      </p>
-      <p>
-        <strong>minFee: </strong><ContractData contract="SportsBook" method="minFee" />
-        <strong>minFee: </strong><ContractData contract="SportsBook" method="minBet" />
-        <strong>minFee: </strong><ContractData contract="SportsBook" method="precision" />
-        <strong>minFee: </strong><ContractData contract="SportsBook" method="profit" />
-        <strong>minFee: </strong><ContractData contract="SportsBook" method="totalPool" />
-        <strong>minFee: </strong><ContractData contract="SportsBook" method="SAN_FRANCISCO_49ERS_pool" />
-        <strong>minFee: </strong><ContractData contract="SportsBook" method="KANSAS_CITY_CHIEFS_pool" />
-        <strong>minFee: </strong><ContractData contract="SportsBook" method="SAN_FRANCISCO_49ERS_bettors" />
-        <strong>minFee: </strong><ContractData contract="SportsBook" method="KANSAS_CITY_CHIEFS_bettors" />
-        <strong>minFee: </strong><ContractData contract="SportsBook" method="SAN_FRANCISCO_49ERS_score" />
-        <strong>minFee: </strong><ContractData contract="SportsBook" method="KANSAS_CITY_CHIEFS_score" />
-      </p>
-      <ContractForm contract="SportsBook" method="set" />
+      <h2>SportsBook Fields</h2>
+      <p><strong>minFee: </strong>
+        <ContractData contract="SportsBook" method="minFee" /> wei</p>
+      <p><strong>minBet: </strong>
+        <ContractData contract="SportsBook" method="minBet" /> wei</p>
+      <p><strong>precision: </strong>
+        <ContractData contract="SportsBook" method="precision" /> wei</p>
+      <p><strong>profit: </strong>
+        <ContractData contract="SportsBook" method="profit" /> wei</p>
+      <p><strong>totalPool: </strong>
+        <ContractData contract="SportsBook" method="totalPool" /> wei</p>
+      <p><strong>SF bettors: </strong>
+        <ContractData contract="SportsBook" method="SAN_FRANCISCO_49ERS_bettors" /> bettors</p>
+      <p><strong>KC bettors: </strong>
+        <ContractData contract="SportsBook" method="KANSAS_CITY_CHIEFS_bettors" /> bettors</p>
     </div>
 
     <div className="section">
-      <h2>SportsBook</h2>
+      <h2>Submit your bets!</h2>
       <p>
         Here we have a form with custom, friendly labels. Also note the token
         symbol will not display a loading indicator. We've suppressed it with
         the <code>hideIndicator</code> prop because we know this variable is
         constant.
       </p>
-      <p>
-        <strong>Total Supply: </strong>
-        <ContractData
-          contract="SportsBook"
-          method="totalSupply"
-          methodArgs={[{ from: accounts[0] }]}
-        />{" "}
-        <ContractData contract="SportsBook" method="symbol" hideIndicator />
-      </p>
-      <p>
-        <strong>My Balance: </strong>
-        <ContractData
-          contract="SportsBook"
-          method="balanceOf"
-          methodArgs={[accounts[0]]}
-        />
-      </p>
-      <h3>Send Tokens</h3>
       <ContractForm
         contract="SportsBook"
-        method="transfer"
-        labels={["To Address", "Amount to Send"]}
+        method="bet"
+        labels={["Team"]}
+        sendArgs={{value:30000000000000000}}
       />
-    </div>
-    <div className="section">
-      <h2>SportsBook</h2>
-      <p>
-        Finally this contract shows data types with additional considerations.
-        Note in the code the strings below are converted from bytes to UTF-8
-        strings and the device data struct is iterated as a list.
-      </p>
-      <p>
-        <strong>String 1: </strong>
-        <ContractData contract="SportsBook" method="string1" toUtf8 />
-      </p>
-      <p>
-        <strong>String 2: </strong>
-        <ContractData contract="SportsBook" method="string2" toUtf8 />
-      </p>
-      <strong>Single Device Data: </strong>
-      <ContractData contract="SportsBook" method="singleDD" />
     </div>
   </div>
 );
