34d87f74fb47188545273fb5d0b034cb8d9ab364
carlosescom <carlos.escom.ipn@gmail.com>
2020-01-31 08:20:41 -0600

    + BN/balanceDifference

--- test/helpers/balanceDifference.js
+++ test/helpers/balanceDifference.js
@@ -1,10 +1,11 @@
 const { ethGetBalance } = require('./web3');
+const BN = require('BN.js');
 
 async function balanceDifference (account, promiseFunc) {
   const balanceBefore = await ethGetBalance(account);
   await promiseFunc();
   const balanceAfter = await ethGetBalance(account);
-  return balanceAfter.sub(balanceBefore);
+  return new BN(balanceAfter,10).sub(new BN(balanceBefore,10));
 }
 
 module.exports = {
