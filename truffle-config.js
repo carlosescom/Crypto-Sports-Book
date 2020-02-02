735573c08cafbdf4cd0d8c513a3c5a4d9a822f88
carlosescom <carlos.escom.ipn@gmail.com>
2020-02-02 01:15:08 -0600

    ParserError: Source file requires different compiler version

--- truffle-config.js
+++ truffle-config.js
@@ -30,5 +30,20 @@ module.exports = {
       },
       network_id: 3
     },
+  },
+  compilers: {
+    solc: {
+      version: "^0.5.0", // A version or constraint - Ex. "^0.5.0"
+                         // Can also be set to "native" to use a native solc
+      docker: false, // Use a version obtained through docker
+      parser: "solcjs",  // Leverages solc-js purely for speedy parsing
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 1000,   // Optimize for how many times you intend to run the code
+        },
+        evmVersion: "petersburg" // Default: "petersburg"
+      }
+    }
   }
 };
